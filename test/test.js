e568cb18c70ac3ce10ca123d7cbc0a4a3f81c3b6
Enrico Marino <enrico.marino@email.com>
2012-04-12 11:07:50 +0200

    added v3.Shader.defaults.fragment

--- test/test.js
+++ test/test.js
@@ -1,1 +1,15 @@
 console.log('test');
+
+var canvas = document.getElementById('v3');
+var context = v3.getContext(canvas);
+var v_shader = new v3.Shader(context, {
+    type: context.VERTEX_SHADER
+  , source: v3.Shader.defaults.vertex
+});
+var f_shader = new v3.Shader(context, {
+    type: context.FRAGMENT_SHADER
+  , source: v3.Shader.defaults.fragment
+});
+
+console.log(v_shader);
+console.log(f_shader);

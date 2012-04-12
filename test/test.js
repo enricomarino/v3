831cc41beee235f9d91cae0bb6c4e26f9f735ad6
Enrico Marino <enrico.marino@email.com>
2012-04-12 11:46:49 +0200

    added v3.Program

--- test/test.js
+++ test/test.js
@@ -10,10 +10,15 @@ var f_shader = v3.Shader.create(context, {
     type: context.FRAGMENT_SHADER
   , source: v3.Shader.defaults.fragment
 });
+var program = new v3.Program(context, {
+    vertex: v_shader
+  , fragment: f_shader
+});
 
-v_shader.destroy(context);
-f_shader.destroy(context);
+// v_shader.destroy(context);
+// f_shader.destroy(context);
 
 console.log(context);
 console.log(v_shader);
 console.log(f_shader);
+console.log(program);

7db2f4d870cf6df3a8fea5ce6d270468c1bf5df1
Enrico Marino <enrico.marino@email.com>
2012-04-12 11:11:33 +0200

    added v3.Shader.create

--- test/test.js
+++ test/test.js
@@ -2,11 +2,11 @@ console.log('test');
 
 var canvas = document.getElementById('v3');
 var context = v3.getContext(canvas);
-var v_shader = new v3.Shader(context, {
+var v_shader = v3.Shader.create(context, {
     type: context.VERTEX_SHADER
   , source: v3.Shader.defaults.vertex
 });
-var f_shader = new v3.Shader(context, {
+var f_shader = v3.Shader.create(context, {
     type: context.FRAGMENT_SHADER
   , source: v3.Shader.defaults.fragment
 });

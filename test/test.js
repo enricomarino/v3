1d429a7015d4a9dd0e5d2639e6c6cb71d546cde7
Enrico Marino <enrico.marino@email.com>
2012-04-13 11:31:48 +0200

    added v3.Model.prototype.bindColors

--- test/test.js
+++ test/test.js
@@ -14,25 +14,34 @@ var program = new v3.Program(context, {
     vertex: v_shader
   , fragment: f_shader
 });
-var attribute = new v3.Attribute({
-    name: 'a_position'
-  , type: context.FLOAT_VEC4
-  , size: 32 * 4
-  , location: -1
-});
-attribute.setIndex(context, program, 1);
 // v_shader.destroy(context);
 // f_shader.destroy(context);
-var uniform = new v3.Uniform(context, {
-    name: 'a_position'
-  , type: context.FLOAT_VEC4
-  , size: 32 * 4
-  , location: -1
-})
+// var attribute = new v3.Attribute({
+//     name: 'a_position'
+//   , type: context.FLOAT_VEC4
+//   , size: 32 * 4
+//   , location: -1
+// });
+// attribute.setIndex(context, program, 1);
+// var uniform = new v3.Uniform(context, {
+//     name: 'a_position'
+//   , type: context.FLOAT_VEC4
+//   , size: 32 * 4
+//   , location: -1
+// });
+
+var model = new v3.Model(context, {
+    vertices: new Float32Array([1,0,0,0,1,0,0,0,1])
+  , colors: new Float32Array([1,0,0,0,0,1,0,0,0,0,1,0])
+});
+
+model.bindVertices(context, program);
+model.bindColors(context, program);
 
 console.log(context);
 console.log(v_shader);
 console.log(f_shader);
 console.log(program);
-console.log(attribute);
-console.log(uniform);
+// console.log(attribute);
+// console.log(uniform);
+console.log(model);
